af81dc3ffbc6f9bd2766c57ac5a917c545e6c863
yang.gao <yang.gao@wuage.com>
2017-10-20 14:17:20 +0800

    update

---
+++ src/main/java/mia/Constant.java
@@ -0,0 +1,12 @@
+package mia;
+
+import java.io.File;
+
+public class Constant {
+
+	public final static String FILE_NAME = "." + File.separator + "files"
+			+ File.separator + "intro.csv";
+
+	public final static String FILE_NAME_UB_BASE = "." + File.separator
+			+ "files" + File.separator + "ub.base";
+}
---
+++ src/main/java/mia/recommender/ch02/EvaluatorIntro.java
@@ -0,0 +1,49 @@
+package mia.recommender.ch02;
+
+import java.io.File;
+
+import mia.Constant;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.RandomUtils;
+
+class EvaluatorIntro {
+
+	private EvaluatorIntro() {
+	}
+
+	public static void main(String[] args) throws Exception {
+		RandomUtils.useTestSeed();
+		DataModel model = new FileDataModel(new File(Constant.FILE_NAME_UB_BASE));
+
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		// Build the same recommender for testing that we did last time:
+		RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
+			@Override
+			public Recommender buildRecommender(DataModel model)
+					throws TasteException {
+				UserSimilarity similarity = new PearsonCorrelationSimilarity(
+						model);
+				UserNeighborhood neighborhood = new NearestNUserNeighborhood(2,
+						similarity, model);
+				return new GenericUserBasedRecommender(model, neighborhood,
+						similarity);
+			}
+		};
+		// Use 70% of the data to train; test using the other 30%.
+		double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7,
+				1.0);
+		System.out.println(score);
+	}
+}
